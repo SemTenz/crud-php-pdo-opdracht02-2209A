63c0d3c4b8d357ea2c4c019b2fdd68157c06bb73
SemTenz <sem.jaeger@ziggo.nl>
2023-01-31 12:42:45 +0100

    second

--- index.php
+++ index.php
@@ -9,37 +9,42 @@
     <title>PDO CRUD</title>
 </head>
 <body>
-    <h1>PDO CRUD</h1>
+    <h1>Maak je eigen pizza</h1>
 
     <form action="create.php" method="post">
-        <label for="firstname">Voornaam:</label><br>
-        <input type="text" name="firstname" id="firstname"><br>
+        <label for="bodem">Bodemformaat</label><br>
+        <select name="centimeter" id="centimeter">
+            <option value="maak je keuze">Maak je keuze</option>
+            <option value="20">20 cm</option>
+            <option value="25">25 cm</option>
+            <option value="30">30 cm</option>
+            <option value="35">35 cm</option>
+            <option value="40">40 cm</option>
+        </select><br><br>
 
-        <label for="infix">Tussenvoegsel:</label><br>
-        <input type="text" name="infix" id="infix"><br>
+        <label for="saus">Saus</label><br>
+        <select name="centimeter" id="centimeter">
+            <option value="keuze">Maak je keuze</option>
+            <option value="tomatensaus">Tomatensaus</option>
+            <option value="extratomatensaus">Extra Tomatensaus</option>
+            <option value="spicyTomatensaus">Spicy Tomatensaus</option>
+            <option value="bbqsaus">BBQ saus</option>
+            <option value="cremefraiche">Créme fraiche</option>
+        </select><br><br>
 
-        <label for="lastname">Achternaam:</label><br>
-        <input type="text" name="lastname" id="lastname"><br>
+        <label for="pizzatoppings">Pizzatoppings</label><br>
+        <input type="radio" name="vegan" id="vegan">
+        <label for="vegan">vegan</label><br>
+        <input type="radio" name="vegetarisch" id="vegetarisch">
+        <label for="vegetarisch">vegetarisch</label><br>
+        <input type="radio" name="vlees" id="vlees">
+        <label for="vlees">vlees</label><br><br>
         
-        <label for="phone">Telefoonnummer:</label><br>
-        <input type="tel" name="phone" id="phone"><br>
+        <label for="kruiden">Kruiden</label><br>
+        <input type="checkbox" name="peterselie" id="peterselie">
+        <label for="peterselie">Peterselie</label><br><br>
 
-        <label for="street">Straatnaam:</label><br>
-        <input type="text" name="street" id="street"><br>
-
-        <label for="number">Huisnummer:</label><br>
-        <input type="text" name="number" id="number"><br>
-
-        <label for="house">Woonplaats:</label><br>
-        <input type="text" name="house" id="house"><br>
-
-        <label for="postcode">Postcode:</label><br>
-        <input type="text" name="postcode" id="postcode"><br>
-
-        <label for="land">Landnaam:</label><br>
-        <input type="text" name="land" id="land"><br>
-
-        <input type="submit" value="Verstuur">
+        <input type="submit" value="Bestel">
     </form>
 </body>
 </html>
